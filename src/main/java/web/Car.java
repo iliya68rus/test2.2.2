83706dae0744f8a3c199b09e2ecd9609d5f949da
iliya68rus <iliya97pro@yandex.ru>
2022-05-02 21:14:53 +0300

    add cars controller and page

---
+++ src/main/java/web/Car.java
@@ -0,0 +1,40 @@
+package web;
+
+public class Car {
+    String model;
+    String color;
+    int series;
+
+    public Car(String model, String color, int series) {
+        this.model = model;
+        this.color = color;
+        this.series = series;
+    }
+
+    public Car() {
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getSeries() {
+        return series;
+    }
+
+    public void setSeries(int series) {
+        this.series = series;
+    }
+}
---
+++ src/main/java/web/controller/CarsController.java
@@ -0,0 +1,34 @@
+package web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import web.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class CarsController {
+
+    @GetMapping("/cars")
+    public String getCars(@RequestParam(value = "count", required = false) Integer count, Model model) {
+        List<Car> cars = new ArrayList<>();
+        cars.add(new Car("Honda", "Red", 211));
+        cars.add(new Car("Mazda", "Green", 137));
+        cars.add(new Car("Nissan", "Blue", 421));
+        cars.add(new Car("BMW", "Pink", 963));
+        cars.add(new Car("Volkswagen", "Brown", 174));
+        if (count == null) {
+            model.addAttribute("cars", cars);
+        } else {
+            List<Car> list = new ArrayList<>();
+            for (int i = 0; i < Math.min(cars.size(), count); i++) {
+                list.add(cars.get(i));
+            }
+            model.addAttribute("cars", list);
+        }
+        return "cars";
+    }
+}
